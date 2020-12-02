16df541363d0b384588be8e1c796543f1fc888ce
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2020-12-02 11:24:18 +0100

    videogameController fixed

--- server/src/controllers/videogameController.js
+++ server/src/controllers/videogameController.js
@@ -1,6 +1,6 @@
 function videogameController(Videogame) {
   function getMethod(req, res) {
-    const query = {};
+    const query = { id: +req.query.id };
     Videogame.findOne(query, (errorGetVideogame, videogame) => ((errorGetVideogame)
       ? res.send(errorGetVideogame) : res.json(videogame)));
   }
