d77b1d2f504edede2c3794d8d8c4f2da6d881b56
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2020-12-08 19:00:16 +0100

    logic description, pictures and comments added

--- press-start/src/app/detail/detail.component.ts
+++ press-start/src/app/detail/detail.component.ts
@@ -21,6 +21,10 @@ export class DetailComponent {
 
   coverIndex:number = 0;
 
+  toggleDescription: boolean = false;
+  togglePictures: boolean = false;
+  toggleComments: boolean = false;
+
   constructor(
     public route: ActivatedRoute,
     public videogameService: VideogameService
@@ -42,5 +46,26 @@ export class DetailComponent {
         this.coverIndex = 0;
         break;
     }
+  }
+
+  seeDescription(): any {
+    this.toggleDescription = !this.toggleDescription;
+    const descriptionOn = <HTMLDivElement>document.getElementById('see-description')
+    this.toggleDescription ? descriptionOn.style.display = 'block' : descriptionOn.style.display = 'none';
+    this.toggleDescription = false;
+  }
+
+  seePictures(): any {
+    this.togglePictures = !this.togglePictures;
+    const picturesOn = <HTMLDivElement>document.getElementById('see-pictures');
+    this.togglePictures ? picturesOn.style.display = 'block' : picturesOn.style.display = 'none';
+    this.togglePictures = false;
+  }
+
+  seeComments(): any {
+    this.toggleComments = !this.toggleComments;
+    const commentsOn = <HTMLDivElement>document.getElementById('see-comments');
+    this.toggleComments ? commentsOn.style.display = 'block' : commentsOn.style.display = 'none';
+    this.toggleComments = false;
   }
 }
